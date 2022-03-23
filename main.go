952e51af3e45b9d34a2329e66ca2621c1801c729
lhp9916 <lhp9916@gmail.com>
2022-03-23 10:03:08 +0800

    读取配置文件

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	btsConfig.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	router := gin.New()
 	bootstrap.SetRouter(router)
-	err := router.Run(":8000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
